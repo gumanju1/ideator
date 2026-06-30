5de8479f1ee4445ceb1e6ea667c16eedf3a7bae9
sweta <sweeta.gumanju@gmail.com>
2026-06-30 15:08:20 +0545

    ideas controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,8 @@
 class IdeasController < ApplicationController
   def index
-    @ideas= Idea.all
+    @ideas = Idea.all
+  end
+  def create
+      @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
   end
 end
