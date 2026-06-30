5bd90a92a4536689ef80f35422ff96df1ed8a6e2
sweta <sweeta.gumanju@gmail.com>
2026-06-30 16:11:02 +0545

    replacing ideas[:params] with idea_params

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,6 +3,12 @@ class IdeasController < ApplicationController
     @ideas = Idea.all
   end
   def create
-      @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
+      @idea = Idea.create(idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:description, :author)
   end
 end
